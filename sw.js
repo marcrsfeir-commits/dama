2111b686912d832f0a1d22e8fcafc55ce4eff7a7
Marc Feir <marcrsfeir@gmail.com>
2026-08-18 19:43:45 +0000

    House rule: must take a capture that eats a dama (king) when available (cache v21)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Dama — offline service worker
-const CACHE = 'dama-v20';
+const CACHE = 'dama-v21';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', function (e) {
